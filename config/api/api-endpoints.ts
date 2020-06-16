45d5564bf968af06dd2c61131edec3b9c3260305
James Abad <james@opnetwork.gg>
2020-06-16 09:35:45 +0300

    Fix attempt for Vercel deployment

--- config/api/api-endpoints.ts
+++ config/api/api-endpoints.ts
@@ -1,6 +1,7 @@
 import { BASE_API_URL } from "./api-base"
 
-const currentUserEndpoint = `${BASE_API_URL}/api/users/current-user`
-const userListEndpoint = `${BASE_API_URL}/api/users`
+const currentUserEndpoint =
+    process.env.NODE_ENV === "development" ? `${BASE_API_URL}/api/users/current-user` : `/api/users/current-user`
+const userListEndpoint = process.env.NODE_ENV === "development" ? `${BASE_API_URL}/api/users` : `/api/users`
 
 export { currentUserEndpoint, userListEndpoint }
