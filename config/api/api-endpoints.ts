aa04af915a54d42745617090a28adfb313c54b6a
James Abad <james@opnetwork.gg>
2020-06-16 10:01:15 +0300

    console.log({ BASE_API_URL })

--- config/api/api-endpoints.ts
+++ config/api/api-endpoints.ts
@@ -1,6 +1,6 @@
 import { BASE_API_URL } from "./api-base"
 
-console.log(BASE_API_URL)
+console.log({ BASE_API_URL })
 
 const currentUserEndpoint = `${BASE_API_URL}/api/users/current-user`
 const userListEndpoint = `${BASE_API_URL}/api/users`
